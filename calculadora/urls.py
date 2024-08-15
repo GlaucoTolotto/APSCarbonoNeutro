4e74575cd4515dd833e0564d00ac56b576bef7c4
GlaucoTolotto <glaucotolotto@gmail.com>
2024-08-15 19:13:11 -0300

    Refatorando projeto antigo - Retirando codigo Django nao utilizado

--- calculadora/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from calculadora.views import home, desenvolvedores
-
-urlpatterns = [
-    path('', home, name=''),
-    path('desenvolvedores/', desenvolvedores, name='desenvolvedores'),
-]
--- calculadora/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'calculadora/pages/home.html')
-
-
-def desenvolvedores(request):
-    return render(request, 'calculadora/pages/desenvolvedores.html')
